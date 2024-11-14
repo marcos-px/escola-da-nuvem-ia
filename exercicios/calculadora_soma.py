a8f9a442ec449d349a4c2567de0cb0aeb1bb1b7a
Marcos Teixeira <marcos.teixeira@nscreen.com.br>
2024-11-14 19:20:24 -0300

    Feat: Adicionando ponto final no texto

--- exercicios/calculadora_soma.py
+++ exercicios/calculadora_soma.py
@@ -5,4 +5,4 @@ numero2 = float(input("Digite o segundo número: "))
 
 soma = numero1 + numero2
 
-print(f"A soma do {numero1} e do {numero2} é {soma} ")
+print(f"A soma do {numero1} e do {numero2} é {soma} .")
