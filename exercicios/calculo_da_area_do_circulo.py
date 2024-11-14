5f070cbd2ff7dbe4096ab0c7ffcfe87b07e798d2
Marcos Teixeira <marcos.teixeira@nscreen.com.br>
2024-11-14 19:18:31 -0300

    Feat: Conteúdo da aula 01

---
+++ exercicios/calculo_da_area_do_circulo.py
@@ -0,0 +1,9 @@
+#Cálculo da Área do Círculo
+
+import math
+
+raio = float(input("Escreva o raio a ser calculado :"))
+
+area = math.pi * (raio ** 2)
+
+print(f"A área do círculo com o raio {raio} é {area:.4f}")
---
+++ exercicios/calculo_do_produto.py
@@ -0,0 +1,8 @@
+#Cálculo do Produto
+
+numero1 = int(input("Coloque o primeiro número : "))
+numero2 = int(input("Coloque o segundo número : "))
+
+produto = numero1 * numero2
+
+print(f"PROD = {produto}")
