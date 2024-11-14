12b0ddbeadafae046322a546d698a12478f0a4f6
Marcos Teixeira <marcos.teixeira@nscreen.com.br>
2024-11-14 19:34:03 -0300

    Feat: Criando Operadores Aritméticos

---
+++ exercicios/aula 02/operadores_aritmeticos.py
@@ -0,0 +1,9 @@
+a, b = 10, 3
+
+print(f"Adição: {a + b} ")
+print(f"Subtração: {a - b} ")
+print(f"Multiplicação: {a * b} ")
+print(f"Divisão: {a / b:.3f} ")
+print(f"Divisão Inteira: {a // b} ")
+print(f"Módulo: {a % b} ")
+print(f"Exponenciação: {a ** b} ")
