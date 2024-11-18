8ab27ccdc584a5f6788b5b4ad13d0a7a096ae88c
Marcos Teixeira <marcos.teixeira@nscreen.com.br>
2024-11-18 19:42:43 -0300

    Feat: Sobe o commit inicial da aula 03

--- exercicios/aula 02/calculo_simples.py
+++ exercicios/aula 02/calculo_simples.py
@@ -2,6 +2,7 @@ cod1, qtd1, valor1 = input(" Digite o código, quantidade e valor do primeiro it
 cod2, qtd2, valor2 = input(" Digite o código, quantidade e valor do segundo item: ").split()
 
 qtd1, valor1 = int(qtd1), float(valor1)
+
 qtd2, valor2 = int(qtd2), float(valor2)
 
 valor_total = (qtd1 * valor1) + (qtd2 * valor2)
